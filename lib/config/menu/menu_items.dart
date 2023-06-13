2faa43b6123033d54a1caa94b9c8088edc59a69b
Fernando Munoz <fernando1986caballeria@gmail.com>
2023-06-13 11:37:10 -0500

    agregamos mitems

---
+++ lib/config/menu/menu_items.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+
+class MenuItem {
+  final String title;
+  final String subTitle;
+  final String link;
+  final IconData Icon;
+
+  const MenuItem(
+      {required this.title,
+      required this.subTitle,
+      required this.link,
+      required this.Icon});
+}
+
+const appMenuItems = <MenuItem>[
+  MenuItem(
+    title: 'Botones', 
+    subTitle: 'varios botones', 
+    link: '/bottons', 
+    Icon: Icons.smart_button_outlined),
+  MenuItem(
+    title: 'Tarjetas', 
+    subTitle: 'Contenedor', 
+    link: '/card', 
+    Icon: Icons.credit_card)
+];
